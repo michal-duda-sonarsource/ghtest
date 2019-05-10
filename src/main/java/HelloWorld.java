9edd535bb04931a2e42e9ae46fd464158f8521f1
Michal Duda <michal.duda@sonarsource.com>
2019-05-10 15:10:49 +0200

    commit on pr

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
 
     System.out.println("yet another change 2");
     System.out.println("yet another change 3");
+    System.out.println("yet another change 4");
   }
 }
