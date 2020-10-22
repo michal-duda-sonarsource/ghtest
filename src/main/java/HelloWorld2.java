2fc5d0ceb24abe0fcf785b14610022e5b57a2f50
Michal Duda <michal.duda@sonarsource.com>
2020-10-22 10:59:02 +0200

    commit on pr

--- src/main/java/HelloWorld2.java
+++ src/main/java/HelloWorld2.java
@@ -1,5 +1,8 @@
 public class HelloWorld2 {
   public void doSomething() {
     System.out.println("abc");
+    System.out.println("abc");
+    System.out.println("abc");
+    System.out.println("abc");
   }
 }
